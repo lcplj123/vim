cc3426228c2c0d7498973a72e0276ade1b413e5a
lcplj123 <lcplj123@163.com>
2014-11-28 23:40:07 +0800

    fix bug about keepalived_install.sh

--- keepalived_install.sh
+++ keepalived_install.sh
@@ -34,11 +34,11 @@ global_defs {
    router_id LVS_DEVEL  #节点名字标识，主要用于通知中
 }
 
-#脚本的定义
-#vrrp_script chk_http{
-#   script "/home/chk_http.sh" #脚本链接
-#	interval 3  #脚本执行间隔
-#	weight 2  #脚本执行结果导致的优先级变更
+#脚本的定义(只有当keepalived进程挂掉后其他服务器才会接管，所以通过脚本可以监控其他的进程)
+#vrrp_script chk_http {
+#    script "/home/chk_http.sh" #脚本链接
+#    interval 3  #脚本执行间隔
+#    weight 2  #脚本执行结果导致的优先级变更
 #}
 
 #vrrp 实例
@@ -48,7 +48,7 @@ vrrp_instance VI_1 {
     virtual_router_id 51  #取值范围0-255 相同的组必须一样。其实是mac地址的最后一组数据的取值,如 00-3e-...-78-{VIRTUAL_ROUTER_ID}
     priority 100  #优先级，如果两个都是BACKUP，则通过优先级决定谁是MASTER
     advert_int 1  #检查间隔，默认1s
-	nopreempt  #设置位不抢占
+    nopreempt  #设置位不抢占 只设定优先级比较高的BACKUP服务器即可
     authentication {  #设置认证
         auth_type PASS  #认证方式
         auth_pass 1111  #密码
@@ -60,12 +60,12 @@ vrrp_instance VI_1 {
 
 	#脚本检查
 	#keepalived可以发现keepalived进程或者机器宕机等，但不能发现nginx等应用是否挂掉，所以需要脚本检查
-	#track_script{
+	#track_script {
 	#    chk_http
     #}
 
     #设置额外的监控，里面的网卡出现问题都会切换
-	#track_interface{
+	#track_interface {
 	#    eth0
 	# 	 eth1
     #}
